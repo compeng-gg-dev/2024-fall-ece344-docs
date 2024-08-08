d44135f8c9e8b58bbbe8f35e5b7921570b0faa94
Jonathan Eyolfson <jon@eyolfson.com>
2024-08-08 10:13:51 -0400

    Change url.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://compeng-gg-dev.github.io/2024-fall-ece344-docs/',
+  url: 'https://compeng-gg-dev.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/2024-fall-ece344-docs/',
